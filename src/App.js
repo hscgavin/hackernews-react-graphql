f3fd3ca4e2ef4c305361ad6875d7e98ca21be775
Gavin He <hscgavin@gmail.com>
2019-09-22 23:11:32 +1000

    Add auth

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import LinkList from './components/LinkList';
 import CreateLink from './components/CreateLink';
 import Header from './components/Header';
 import { Switch, Route } from 'react-router-dom';
+import Login from './components/Login';
 function App() {
   return (
     <div className="center w85">
@@ -12,6 +13,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={LinkList} />
           <Route exact path="/create" component={CreateLink} />
+          <Route exact path="/login" component={Login} />
         </Switch>
       </div>
     </div>

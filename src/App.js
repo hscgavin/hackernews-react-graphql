479d234a1520a28d066f53a6bfaccce71878cefe
Gavin He <hscgavin@gmail.com>
2019-09-22 12:04:47 +1000

    Create links

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LinkList from './components/LinkList';
+import CreateLink from './components/CreateLink';
 function App() {
-  return <LinkList />;
+  // return <LinkList />;
+  return <CreateLink />;
 }
 
 export default App;

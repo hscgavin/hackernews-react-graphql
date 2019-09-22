2b4b03dff2196d63e1dcde62e886f3761067ffa8
Gavin He <hscgavin@gmail.com>
2019-09-22 12:34:21 +1000

    Add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import LinkList from './components/LinkList';
 import CreateLink from './components/CreateLink';
+import Header from './components/Header';
+import { Switch, Route } from 'react-router-dom';
 function App() {
-  // return <LinkList />;
-  return <CreateLink />;
+  return (
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+        </Switch>
+      </div>
+    </div>
+  );
 }
 
 export default App;

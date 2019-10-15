48151db95147f2543aea3a35c128f68d96ad237e
Gavin He <hscgavin@gmail.com>
2019-10-15 11:13:57 +1100

    Add search

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CreateLink from './components/CreateLink';
 import Header from './components/Header';
 import { Switch, Route } from 'react-router-dom';
 import Login from './components/Login';
+import Search from './components/Search';
 function App() {
   return (
     <div className="center w85">
@@ -14,6 +15,7 @@ function App() {
           <Route exact path="/" component={LinkList} />
           <Route exact path="/create" component={CreateLink} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/search" component={Search} />
         </Switch>
       </div>
     </div>
